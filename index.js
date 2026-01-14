95135fccc07f826f40f5e1647ce7f5ae5001b6a8
Mergen Bauyrzhan <mambablackright@gmail.com>
2026-01-14 21:09:10 +0500

    Many 21's were added into the main object

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function isTwentyOne (arg) {
-		
+	
+	if (typeof arg == "string") {
+		arg = arg.toLowerCase();
+	}
 	// this object must contain twenty one's as the keys and the string description of each key must be values of the given object  
 	let twentyOneS = {
 		 "twenty one": "21 written in English",
@@ -12,16 +15,22 @@ function isTwentyOne (arg) {
 		"7*3": "twenty one prime factorized and represented as a string",
 		"7x3": "twenty one prime factorized and represented as a string",
 		"10101": "twenty one in binary in 5-bit representation",
-		"0001 0101": "twenty one in binary in 8-bit representation with space in the middle between bytes",
+		"0001 0101": "twenty one in binary in 8-bit representation with space in the middle",
 		"00010101": "twenty one in binary in 8-bit representation",
+		"dudek uno": "twenty one in Esperanto",
+		"73": "21st prime number",
+		73: "21st prime number",
+		"linux": "the age Linus Torvalds started working on Linux Kernel at",
+		"12": "reversed 21 in arabic numerals",
+		 12: "reversed 21 in arabic numerals",
+		"IXX": "reversed 21 in roman numerals",
 		
-
-
 	};
-	return `${arg}: twentyOneS[arg]`;
+	return `${arg}: ${twentyOneS[arg]}`;
 	
 
 }
+
 
 
 
